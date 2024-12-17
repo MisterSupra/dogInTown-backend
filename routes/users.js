5eb8af502d055a9434381722068decf5dbafc86d
Florian <florian.soulas1@gmail.com>
2024-12-17 11:28:47 +0100

    modif connexion

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.post('/connection', (req, res) => {
 
   User.findOne({ email: req.body.email }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token, username: data.username });
+      res.json({ result: true, token: data.token, username: data.username, dogs: data.dogs });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
